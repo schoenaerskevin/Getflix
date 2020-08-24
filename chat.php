418b4e2d3ea537203d83ba04b1c6cab9f751e52a
MeunierS <meuniersebastien1987@gmail.com>
2020-08-24 16:51:40 +0200

    add chat section

---
+++ chat.php
@@ -0,0 +1,47 @@
+<!--form to add message to chat-->
+<form method="POST" action="chatpost.php" class="form-group"> 
+<label for="pseudo">pseudo</label>
+<input type="text" name="pseudo" class="form-control">
+<label for="chat">Text</label>
+<input type="text" name="chat" class="form-control">
+<input class="btn btn-primary" type="submit" name="submit" value="Post">
+</form> 
+
+<?php 
+// * done by Seb
+// Connect to database
+try
+{
+	$bdd = new PDO('mysql:host=database;dbname=Streamler;charset=utf8', 'root', 'root');
+}
+//error
+catch(Exception $e)
+{
+        die('Error : '.$e->getMessage());
+}
+// last 20 comments
+$req = $bdd->query('SELECT ID, pseudo, comment FROM Comment ORDER BY ID DESC LIMIT 0, 20');
+
+while ($donnees = $req->fetch())
+{
+?>
+<div class="comment">
+    <h3 class="pseudochat">
+        <?php
+        //pseudo from db
+        echo htmlspecialchars($donnees['pseudo']); ?>
+    </h3>
+    
+    <p class="chat">
+    <?php
+    //comment from db
+    echo nl2br(htmlspecialchars($donnees['comment']));
+    ?>
+    </p>
+</div>
+<?php
+} 
+// end while for comment
+$req->closeCursor();
+?>
+
