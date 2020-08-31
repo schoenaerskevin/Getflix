2aa7f6e5920e9b23ad7e02ee55e22f3541f11ee4
MeunierS <meuniersebastien1987@gmail.com>
2020-08-31 14:26:55 +0200

    emoticone chat + if premium chat

--- index.php
+++ index.php
@@ -5,9 +5,23 @@
 	$id_session = session_id();
 
 	
+// * done by Seb
+// Connect to database
+try
+{
+	$bdd = new PDO('mysql:host=database;dbname=streamler', 'root', 'root');
+}
+//error
+catch(Exception $e)
+{
+        die('Error : '.$e->getMessage());
+}
 
+$req = $bdd->prepare("SELECT * FROM user WHERE pseudo = ?");
+$req->execute(array($_SESSION['pseudo']));
+$donnees = $req-> fetch();
 
- ?>
+?>
  
  <!DOCTYPE html>
  <html lang="fr">
@@ -18,9 +32,10 @@
  <body>
    <?php 
     	include 'menu.php';
-      include 'home.php';
-      include 'chat.php';
-
+	  include 'home.php';
+	  if ($donnees['droit']=="premium" || $donnees['droit']=="admin"){
+      	include 'chat.php';
+	}
  	 ?>
  </body>
  </html>
