0d4dab9dad4565322431c30eb9c3de83a18b3292
Kevin S <schoenaerskevin@gmail.com>
2020-09-03 11:07:34 +0200

    duplication chat.php

--- dbreq.php
+++ dbreq.php
@@ -7,8 +7,9 @@
 try
 {
     //! mise en comment de la ligne qui ne correspond pas
-    $bdd = new PDO('mysql:host=database;dbname=streamler', 'root', 'root');
+    //$bdd = new PDO('mysql:host=database;dbname=streamler', 'root', 'root');
     //$bdd = new PDO('mysql:host=localhost;dbname=streamler;charset=utf8', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=152340', '152340', 'Streamlerfreeweb');
 }
 //error
 catch(Exception $e)
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//refresh chat div
-function refreshTimer() {
-    new Ajax.PeriodicalUpdater('chatBox', '/chat.php', {
-       method: 'post', frequency: 3, decay: 2
-    });
- }
+$(document).ready(function() { 
+   $("#chatBox.php").load("chatbox.php"); 
+   
