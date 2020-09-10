8276b9f0330c4b463bc353a69700b49833d705b0
Olivier Peeters <olivierpeeters73@gmail.com>
2020-09-10 15:03:26 +0200

    repair connexion (#35)

--- connexion.php
+++ connexion.php
@@ -9,8 +9,9 @@ if(isset($_POST['formconnexion'])) {
       $requser1 = $bdd->prepare("SELECT * FROM mdp WHERE mdp = ? ");
       $requser1->execute(array( $mdpconnect));
       
-      $userexist = $requser->rowCount();
-      if($userexist == 1) {
+	  $userexist = $requser->rowCount();
+	  $mdpexist = $requser1->rowCount();
+      if($userexist == 1 && $mdpexist==1) {
          $userinfo = $requser->fetch();
          $_SESSION['id'] = $userinfo['id'];
          $_SESSION['pseudo'] = $userinfo['pseudo'];
